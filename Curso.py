1f3fe1f0f495c972074a0ce906112d7dc342225f
Xondet <davidoe31@gmail.com>
2024-04-11 09:33:07 -0500

    first commit

---
+++ Curso.py
@@ -0,0 +1,17 @@
+class Curso:
+  __MINIMA=1.5
+  __MAXIMA=5.0
+def __init__(self):
+    self.codigo=0
+    self.nombre=""
+    self.departamento=""
+    self.creditos=0
+    self.nota=0.0
+def Nombre_Curso(self):
+   self.nombre== "Calculo 1"
+def Nota_Curso(self):
+   self.nota >=0.0
+def NumeroCreditos(self):
+   self.creditos > 3
+def CursoAprobado(self):
+   self.nota >= 3.25
